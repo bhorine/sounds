7ba88d49a48db8dfda4d26a4adca3c8704541fe1
brent.horine <brent.horine@manhattan.edu>
2017-09-16 21:26:26 -0400

    added culexrestuans

--- README.md
+++
@@ -1,2 +0,0 @@
-This repository includes sounds for wildlife including mosquitos and quail.
-Most sounds are recorded at 44100 Hz sample rate and each species is in its own folder.
--- mosquitosoundsamples/PlotSpectrum.m
+++
@@ -1,8 +0,0 @@
-function PlotSpectrum(audio, Fs)
-  NFFT = 2^nextpow2(size(audio, 1));
-  freqs = Fs/NFFT*(0:(NFFT/2 - 1));
-  spect = fft(audio, NFFT);
-  psd = [spect(:,1).*conj(spect(:,1)) spect(:,2).*conj(spect(:,2))]/NFFT;
-  figure,plot(freqs, 20.0*log10(psd(1:NFFT/2,1)), freqs, 20.0*log10(psd(1:NFFT/2,2)))
-  xlabel('Frequency (Hz)');
-  ylabel('PSD (dB)');
